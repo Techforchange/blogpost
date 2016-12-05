10af5fcbceb65c6f3cc831ef0b24bdbcdcc2113c
ArlenePerez <arleneperezsb@gmail.com>
2016-12-04 17:07:10 -0800

    Forces the user to redirect to the login page if user is not logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # before_action :authenticate_user!
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
 end
